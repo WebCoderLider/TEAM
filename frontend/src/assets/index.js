8644d9045f32010e1e8fc97cee808200f5961dbe
shokhruzisroilov <shokhruzisroilov@gmail.com>
2023-11-02 15:45:34 +0500

    course-info is done

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,5 @@
+import onlineTest from './icons/online-test-icon.png'
+import examIcon from './icons/exam-icon.png'
+import certificationIcon from './icons/certification-icon.png'
+
+export { onlineTest, examIcon, certificationIcon }
