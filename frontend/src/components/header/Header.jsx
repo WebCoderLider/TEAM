6b36434ea1cf779b35da8797f389ed126f8816c8
AbdullohKomil <akomiljonov@icloud.com>
2023-11-02 11:52:20 +0500

    edited header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,5 +1,37 @@
+import './header.scss'
+
 const Header = () => {
-	return <div>Header</div>
+  return (
+    <header className='header'>
+      <div className="container">
+				<nav className='d-flex '>
+          <ul className='d-flex gap-3 '>
+            <li>
+              <a className="text-black " href="#">
+                Asosiy
+              </a>
+            </li>
+            <li>
+              <a className="text-black " href="#">
+                Kurslar
+              </a>
+            </li>
+            <li>
+              <a className="text-black " href="#">
+                Servislar
+              </a>
+            </li>
+            <li>
+              <a className="text-black " href="#">
+                Bog'lanish
+              </a>
+            </li>
+          </ul>
+          <a href="">Kirish</a>
+        </nav>
+      </div>
+    </header>
+  )
 }
 
 export default Header
