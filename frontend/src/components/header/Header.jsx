8317063d38485374b8fe59b1d5ad53e28afc5ac9
AbdullohKomil <akomiljonov@icloud.com>
2023-11-02 12:20:46 +0500

    header end

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -5,29 +5,29 @@ const Header = () => {
     <header className='header'>
       <div className="container">
 				<nav className='d-flex '>
-          <ul className='d-flex gap-3 '>
+          <ul className='d-flex gap-3 align-items-center '>
             <li>
-              <a className="text-black " href="#">
+              <a className="text-black header__navbar__links" href="#">
                 Asosiy
               </a>
             </li>
             <li>
-              <a className="text-black " href="#">
+              <a className="text-black header__navbar__links" href="#">
                 Kurslar
               </a>
             </li>
             <li>
-              <a className="text-black " href="#">
+              <a className="text-black header__navbar__links" href="#">
                 Servislar
               </a>
             </li>
             <li>
-              <a className="text-black " href="#">
+              <a className="text-black header__navbar__links" href="#">
                 Bog'lanish
               </a>
             </li>
           </ul>
-          <a href="">Kirish</a>
+          <a href="" className='header__login__link'>Kirish</a>
         </nav>
       </div>
     </header>
